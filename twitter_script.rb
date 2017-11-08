49785c59590edacc67cbf0483615f8cb0f880ae7
Noah Berman <noah@wetransfer.com>
2017-11-08 16:09:05 +0100

    Add book writing thing

--- twitter_script.rb
+++ twitter_script.rb
@@ -9,18 +9,44 @@ Dotenv.load
 # Documentation for the twitter gem we're using can be found here:
 # http://www.rubydoc.info/gems/twitter
 
-# Loads all the keys needed to access twitter's API, then creates a client
-# that you can use to post, delete, reply, etc.
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+
+class WriteABook
+def self.write_book(client, topics)
+  collected_tweets = []
+  tweet_getter(collected_tweets, client, topics)
 end
 
-message = "Testing, testing"
+  def self.tweet_getter(collected_tweets, client, topics)
+    client.filter(track: topics.join(",")) do |object|
+      collected_tweets << object.text if object.is_a?(Twitter::Tweet)
+      if collected_tweets.count >= 3000
+        puts collected_tweets.count
+        book_writer(collected_tweets)
+        break
+      else
+        puts collected_tweets.count
+      end
+    end
+  end
 
-puts "Sending tweet: #{message}"
+  def self.book_writer(collected_tweets)
+    File.open("test.txt", "w+") do |f|
+      collected_tweets.each { |element| f.puts(element) }
+    end
+  end
+end
 
-# This posts the string you have in the message variable to your twitter account!
-client.update(message)
+topics = ["stranger","champions",
+  "barcelona","ruby","politics","unicorns",
+  "phenomena","christmas","winter","awesome"]
+
+  # Loads all the keys needed to access twitter's API, then creates a client
+  # that you can use to post, delete, reply, etc.
+  client = Twitter::Streaming::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
+
+WriteABook.write_book(client, topics)
